8529db575fd063411f10c60c1f977df91c61e52f
Gareth Lacey <gareth_1992@live.com>
2017-08-03 00:25:09 +0100

    added src files

---
+++ src/main/java/com/gsx/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.gsx.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "index";
+	}
+
+}
