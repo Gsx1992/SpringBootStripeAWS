53aa2527a5cc6fef7c29814e7a26b9695213e1fc
Gareth Lacey <gareth_1992@live.com>
2017-08-04 23:26:55 +0100

    added i18n support

--- src/test/java/com/gsx/DevopsbuddyApplicationTests.java
+++ src/test/java/com/gsx/DevopsbuddyApplicationTests.java
@@ -1,16 +1,29 @@
 package com.gsx;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.gsx.web.i18n.I18NService;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18NService i18nService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18nService.getMessage(messageId);
+		Assert.assertEquals("Test fail, no match", expectedResult, actual);
 	}
 
 }
